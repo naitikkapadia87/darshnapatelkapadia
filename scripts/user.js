4b7f0abef4304ffba4886dd4af9c24f635955bf0
Henry Heng <siekhaiheng@gmail.com>
2020-11-22 15:59:10 +0700

    Fixed countdown timer

--- scripts/user.js
+++ scripts/user.js
@@ -1,5 +1,12 @@
 $('#example').countdown({
     date: '12/23/2020 00:00:00'
+    // date: '11/20/2020 00:00:00'
 }, function () {
-    //alert('Merry Christmas!');
+    // alert('Merry Christmas!');
+    var marriedIndicator = 'images/laurel-5.png';
+    if($('#married-status').attr('src') === marriedIndicator){
+    	return;
+    }
+
+    $('#married-status').attr('src', marriedIndicator);
 });
