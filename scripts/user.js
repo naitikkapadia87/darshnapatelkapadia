74725ab214adcc7e13f77903525327d094da26b9
Henry Heng <siekhaiheng@gmail.com>
2020-11-28 21:10:32 +0700

    Added pre-wedding images

--- scripts/user.js
+++ scripts/user.js
@@ -10,3 +10,27 @@ $('#example').countdown({
 
     $('#married-status').attr('src', marriedIndicator);
 });
+
+
+function loadGallery(){
+    var currentIndexHtmlFilepath = window.location.href;
+    var indexHtmlBaseUrl = currentIndexHtmlFilepath.substring( 0, currentIndexHtmlFilepath.lastIndexOf( "/" ) + 1);
+    $.getJSON(indexHtmlBaseUrl +  "/images/gallery/manifest.json", function(data){
+        if (!Array.isArray(data)){
+            console.log("Gallery manifest file is not array! Check the format of the manifest content at: " + indexHtmlBaseUrl +  "/images/gallery/manifest.json");
+            return;
+        }
+
+        data.forEach(function (d) {
+            console.log(d);
+            var img = `<div class="card" data-groups="[`+ d.group +`]"> <a href="images/gallery/`+ d.filename +`" data-toggle="lightbox" data-gallery="ww-gallery"> <img class="img-fluid" src="images/gallery/`+ d.filename + `" alt=" `+ d.filename +`" /> </a> </div>`;
+            $('#gallery-to-append').append(img);
+
+        })
+    }).fail(function(err){
+        console.log("An error has occurred. Response: " +  err);
+        console.log(err);
+    });
+}
+
+loadGallery();
